0a7e851a1a7d0eec01c3582afaeb4cb2a83792e6
ccastro1016 <andycastro.001016@gmail.com>
2024-04-14 18:09:11 -0500

    testing firebase

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 // Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection } from 'firebase/firestore'
+import { VueFire, VueFireAuth, VueFireFirestoreOptionsAPI } from 'vuefire'
 
 const vuetify = createVuetify({
   components,
@@ -19,11 +22,33 @@ const vuetify = createVuetify({
     defaultTheme: 'light'
   }
 })
+export const firebaseApp = initializeApp({
+  apiKey: "AIzaSyDrCMtsrTuCbnLWj-NaHtxTO1KG_2X1VY0",
+  authDomain: "nominas-vue-ut.firebaseapp.com",
+  projectId: "nominas-vue-ut",
+  storageBucket: "nominas-vue-ut.appspot.com",
+  messagingSenderId: "58400371978",
+  appId: "1:58400371978:web:8c0478505ed10cd8c8b6f3"
+})
+
+// used for the firestore refs
+const db = getFirestore(firebaseApp)
+
+// here we can export reusable database references
+export const todosRef = collection(db, 'todos')
 
 const app = createApp(App)
 
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
-
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: [
+    // we will see other modules later on
+    VueFireAuth(),
+    VueFireFirestoreOptionsAPI(),
+  ],
+})
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import EmployeesView from '../views/EmployeesView.vue'
-import PayrollView from '../views/PayrollView.vue'
-import SettingsView from '../views/SettingsView.vue'
-import PermitView from '../views/PermitView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import EmployeesView from '@/views/EmployeesView.vue'
+import PayrollView from '@/views/PayrollView.vue'
+import SettingsView from '@/views/SettingsView.vue'
+import PermitView from '@/views/PermitView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
